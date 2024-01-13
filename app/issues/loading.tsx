90d7ac9024f768ed06d296122335e86df36a1d79
kranthivr <kranthi.neo1@gmail.com>
2024-01-13 16:22:56 +0530

    Refactor: Organize import statements

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,12 +1,9 @@
 import { Table } from "@radix-ui/themes";
-import React from "react";
-import IssueStatusBadge from "../components/IssueStatusBadge";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
 import IssueActions from "./IssueActions";
+import { Skeleton } from "@/app/components";
 
 const LoadingIssuesPage = () => {
-  const issues = [1, 2, 3.4, 5];
+  const issues = [1, 2, 3, 4, 5];
 
   return (
     <div>
