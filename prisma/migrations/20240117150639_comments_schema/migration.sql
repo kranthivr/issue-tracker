740f1ffbf272cf43e4fc76f4b335c6ffd8da8db9
kranthivr <kranthi.neo1@gmail.com>
2024-01-18 08:37:40 +0530

    dropping migrations

--- prisma/migrations/20240117150639_comments_schema/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE `Comment` (
-    `id` VARCHAR(191) NOT NULL,
-    `comment` VARCHAR(255) NOT NULL,
-    `commentUserId` VARCHAR(255) NOT NULL,
-    `commentIssueId` INTEGER NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
